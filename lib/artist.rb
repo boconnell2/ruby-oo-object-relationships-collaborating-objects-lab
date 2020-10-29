60230be8679c1961521323b5b4eb7ecd961fb912
boconnell2 <bmoconnell15@gmail.com>
2020-10-29 13:40:40 -0400

    commit tree b7f45265d8b697a1e337a4d44a77580517d3440e

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Artist
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_song(song)
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def self.find_or_create_by_name(name)
+        if (self.artist.nil?)
+            self.artist = Artist.new(name)
+        else
+            self.artist.name = name
+        end
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+    attr_accessor :name, :artist
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artist_name=(name)
+        Artist.find_or_create_by_name(name)
+    end
+
+    def self.new_by_filename
+
+    end
+end
